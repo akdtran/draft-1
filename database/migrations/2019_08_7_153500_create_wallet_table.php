d4e3b12c7bf5c356245767932f59e73a079eb61f
akdtran <48376984+akdtran@users.noreply.github.com>
2019-08-07 15:35:28 +0700

    Pipedream scaffolding

---
+++ database/migrations/2019_08_7_153500_create_wallet_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWalletTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            ___SOFT_DELETES_BLOCK___
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallet');
+    }
+}
